3f8b32d8eba59e3eaa87554e4495b7807ca88e09
m00se333 <mauricecunningham@ymail.com>
2016-04-25 15:42:07 -0700

    add routes for all pages in app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
 "use strict";
 
-var express = require("express");
+var express = require("express"),
+	posts = require("./mock/posts.json");
 
 var app = express();
 
 app.get("/", function(req, res){
 	res.send("Making things happen");
 });
+
+app.get("/blog", function(req, res){
+	res.send(posts);
+});
+
+app.get("/about", function(req, res){
+	res.send("<h1>About Page</h1>");
+})
+
+app.get("/projects", function(req, res){
+	res.send("<h1>Projects Page</h1>")
+})
 
 app.listen(4000, function(){
 	console.log("Frontend server is running on port 4000.")
