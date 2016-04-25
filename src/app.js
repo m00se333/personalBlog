55f9ceb319b7c3ca5a91cabd6cbd5bb790c6978f
m00se333 <mauricecunningham@ymail.com>
2016-04-25 14:43:56 -0700

    console message for server running

--- src/app.js
+++ src/app.js
@@ -8,4 +8,6 @@ app.get("/", function(req, res){
 	res.send("Making things happen");
 });
 
-app.listen(4000);
+app.listen(4000, function(){
+	console.log("Frontend server is running on port 4000.")
+});
